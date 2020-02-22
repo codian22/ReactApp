2dd76c6dd3b24704a34066f9afa333985c2ebda8
codian22 <codian.freetime@gmail.com>
2020-02-22 17:28:02 +0900

    it's something about seconde changed.

--- do-something/src/App.tsx
+++ do-something/src/App.tsx
@@ -6,9 +6,14 @@ function App() {
   let message = 'This is test';
 
   return (
+    <>
     <Button variant="contained" color="primary">
       { message }
     </Button>
+    <Button variant="contained" color="primary">
+      { message + '2' }
+    </Button>
+    </>
   );
 }
 
