ab4df06ffb2df2600dd95cc42371cab37782bd85
codian22 <codian.freetime@gmail.com>
2020-02-22 17:12:40 +0900

    It's first set up for project

---
+++ do-something/src/App.tsx
@@ -0,0 +1,45 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  let message = 'This is test';
+
+  return (
+    <Button variant="contained" color="primary">
+      { message }
+    </Button>
+  );
+}
+
+export default App;
+
+// ReactDOM.render(<App />, document.querySelector('#app'));
